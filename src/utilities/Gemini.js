3552585a967b4f93e7f9636327582ee7cf04389e
Hafiz-Muhammad-Furqan <hafizm.furqan123@gmail.com>
2025-05-19 22:20:00 +0500

    Change Gemini Model and solve some bugs

--- src/utilities/Gemini.js
+++ src/utilities/Gemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = import.meta.env.VITE_GEMINIAPIKEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
